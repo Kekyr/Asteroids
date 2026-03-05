52f4f1d102e553cafc2771d393c1e8ed9606ed9d
Mihail Ostronosov <supermanmgdn@gmail.com>
2026-03-06 00:49:19 +0300

    Polish views and add zenject

--- Assets/Game/Scripts/View/LaserGunView.cs
+++ Assets/Game/Scripts/View/LaserGunView.cs
@@ -10,8 +10,12 @@ namespace View
         [SerializeField] private TextMeshProUGUI _shootCountText;
         [SerializeField] private TextMeshProUGUI _coolDownText;
 
+        private LaserGunViewModel _viewModel;
+
         public void Construct(LaserGunViewModel viewModel)
         {
+            _viewModel = viewModel;
+            
             viewModel.ShootCount.Subscribe(x => _shootCountText.text = x).AddTo(this);
             viewModel.CoolDownTime.Subscribe(x => _coolDownText.text = x).AddTo(this);
         }
